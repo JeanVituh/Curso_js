e1ec332efa6ccb57afe6bd56810969bf572e18aa
Jean Vitor <jeanvitormeirelestaio@gmail.com>
2026-02-26 23:22:04 -0300

    Add_aulas

---
+++ curso-js/aula7/operadores_aritmeticos.js
@@ -0,0 +1,14 @@
+/**
+ * Operadores Aritméticos
+ * + Adicao/concatenação
+ * ** Potenciação
+ * % Resto da divisão
+ * Incremento = ++
+ * Decremento = --
+ */
+// NaN - Not a number parseInt (inteiro), parseFloat(decimais)
+const num1 = 10;
+const num2 = Number('Jean');
+
+console.log(num1 + num2);
+console.log(typeof num2);
